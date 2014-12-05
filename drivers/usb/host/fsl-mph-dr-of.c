80df96b97df5751541792b397ced4c0549928c2b
Ramneek Mehresh <ramneek.mehresh@freescale.com>
2014-12-05 17:56:02 +0530

    drivers:usb:gadget: Fix kernel boot dump for usb gadegt drv
    Copy of_node property only for host mode so as to fix gadget mode
    kernel dump messages
    
    Signed-off-by: Ramneek Mehresh <ramneek.mehresh@freescale.com>

--- drivers/usb/host/fsl-mph-dr-of.c
+++ drivers/usb/host/fsl-mph-dr-of.c
@@ -98,7 +98,8 @@ static struct platform_device *fsl_usb2_device_register(
 	pdev->dev.coherent_dma_mask = ofdev->dev.coherent_dma_mask;
 	*pdev->dev.dma_mask = *ofdev->dev.dma_mask;
 
-	pdev->dev.of_node = ofdev->dev.of_node;
+	if (pdata->operating_mode != FSL_USB2_DR_DEVICE)
+		pdev->dev.of_node = ofdev->dev.of_node;
 
 	retval = platform_device_add_data(pdev, pdata, sizeof(*pdata));
 	if (retval)

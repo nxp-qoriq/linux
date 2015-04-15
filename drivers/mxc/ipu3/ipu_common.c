a402ab252f79bb0a921056b008e5790368dcd6f4
Sebastien Laveze <sebastien.laveze@freescale.com>
2015-04-15 14:31:25 +0200

    mxc: ipu3: irq spinlock fix
    Disable local interrupts as the lock is shared between 2 IRQs.
    Do not assume that the irqs are disabled as for threaded irqs or RT enabled
    systems, this is not the case.
    
    Signed-off-by: Marouen Ghodhbane <marouen.ghodhbane@nxp.com>
    Signed-off-by: Hou Zhiqiang <Zhiqiang.Hou@nxp.com>

--- drivers/mxc/ipu3/ipu_common.c
+++ drivers/mxc/ipu3/ipu_common.c
@@ -2921,8 +2921,9 @@ static irqreturn_t ipu_sync_irq_handler(int irq, void *desc)
 	uint32_t line, bit, int_stat, int_ctrl;
 	irqreturn_t result = IRQ_NONE;
 	const int int_reg[] = { 1, 2, 3, 4, 11, 12, 13, 14, 15, 0 };
+	unsigned long flags;
 
-	spin_lock(&ipu->int_reg_spin_lock);
+	spin_lock_irqsave(&ipu->int_reg_spin_lock, flags);
 
 	for (i = 0; int_reg[i] != 0; i++) {
 		int_stat = ipu_cm_read(ipu,
@@ -2947,7 +2948,7 @@ static irqreturn_t ipu_sync_irq_handler(int irq, void *desc)
 		}
 	}
 
-	spin_unlock(&ipu->int_reg_spin_lock);
+	spin_unlock_irqrestore(&ipu->int_reg_spin_lock, flags);
 
 	return result;
 }
@@ -2958,8 +2959,9 @@ static irqreturn_t ipu_err_irq_handler(int irq, void *desc)
 	int i;
 	uint32_t int_stat;
 	const int err_reg[] = { 5, 6, 9, 10, 0 };
+	unsigned long flags;
 
-	spin_lock(&ipu->int_reg_spin_lock);
+	spin_lock_irqsave(&ipu->int_reg_spin_lock, flags);
 
 	for (i = 0; err_reg[i] != 0; i++) {
 		int_stat = ipu_cm_read(ipu,
@@ -2978,7 +2980,7 @@ static irqreturn_t ipu_err_irq_handler(int irq, void *desc)
 		}
 	}
 
-	spin_unlock(&ipu->int_reg_spin_lock);
+	spin_unlock_irqrestore(&ipu->int_reg_spin_lock, flags);
 
 	return IRQ_HANDLED;
 }

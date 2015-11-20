c17d58c463aefaaae628b48f0af5c0fa45f2e401
Rajesh Bhagat <rajesh.bhagat@freescale.com>
2015-11-20 12:45:31 +0530

    usb: dwc3 : Add support for USB snooping
    Add support for USB3 snooping by asserting bits
    in register DWC3_GSBUSCFG0 for data and descriptor
    
    Signed-off-by: Nikhil Badola <nikhil.badola@freescale.com>
    Signed-off-by: Rajesh Bhagat <rajesh.bhagat@freescale.com>
    Signed-off-by: yinbo.zhu <yinbo.zhu@nxp.com>

--- drivers/usb/dwc3/core.c
+++ drivers/usb/dwc3/core.c
@@ -973,6 +973,7 @@ static int dwc3_probe(struct platform_device *pdev)
 {
 	struct device		*dev = &pdev->dev;
 	struct dwc3_platform_data *pdata = dev_get_platdata(dev);
+	struct device_node      *node = dev->of_node;
 	struct resource		*res;
 	struct dwc3		*dwc;
 	u8			lpm_nyet_threshold;
@@ -1171,6 +1172,13 @@ static int dwc3_probe(struct platform_device *pdev)
 		(dwc3_readl(dwc->regs, DWC3_GSBUSCFG0) & ~0xff) | 0xf);
 	dwc3_writel(dwc->regs, DWC3_GSBUSCFG1,
 		dwc3_readl(dwc->regs, DWC3_GSBUSCFG1) | 0xf00);
+
+	/* Enable Snooping */
+	if (node && of_dma_is_coherent(node)) {
+		dwc3_writel(dwc->regs, DWC3_GSBUSCFG0,
+		dwc3_readl(dwc->regs, DWC3_GSBUSCFG0) | 0x22220000);
+		dev_dbg(dev, "enabled snooping for usb\n");
+	}
 
 	if (IS_ENABLED(CONFIG_USB_DWC3_HOST))
 		dwc->dr_mode = USB_DR_MODE_HOST;

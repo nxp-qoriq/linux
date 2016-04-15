8187fc86f96deb9c65bb64de3a72ae1fe8a1cb03
Stuart Yoder <stuart.yoder@nxp.com>
2016-04-15 17:07:16 -0500

    staging: fsl-mc: make fsl_mc_get_root_dprc public
    this is needed by other components (e.g. vfio) to find
    the root dprc
    
    Signed-off-by: Stuart Yoder <stuart.yoder@nxp.com>

--- drivers/staging/fsl-mc/bus/mc-bus.c
+++ drivers/staging/fsl-mc/bus/mc-bus.c
@@ -358,7 +358,7 @@ EXPORT_SYMBOL_GPL(fsl_mc_bus_exists);
 /**
 * fsl_mc_get_root_dprc - function to traverse to the root dprc
 */
-static void fsl_mc_get_root_dprc(struct device *dev,
+void fsl_mc_get_root_dprc(struct device *dev,
 				 struct device **root_dprc_dev)
 {
 	if (WARN_ON(!dev)) {
@@ -371,6 +371,7 @@ static void fsl_mc_get_root_dprc(struct device *dev,
 			*root_dprc_dev = (*root_dprc_dev)->parent;
 	}
 }
+EXPORT_SYMBOL_GPL(fsl_mc_get_root_dprc);
 
 static int get_dprc_attr(struct fsl_mc_io *mc_io,
 			 int container_id, struct dprc_attributes *attr)

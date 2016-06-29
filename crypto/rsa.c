e0ef47b8dd771fd055b4cd5ce69a5825a78c6837
Herbert Xu <herbert@gondor.apana.org.au>
2016-06-29 19:32:21 +0800

    crypto: rsa - Generate fixed-length output
    Every implementation of RSA that we have naturally generates
    output with leading zeroes.  The one and only user of RSA,
    pkcs1pad wants to have those leading zeroes in place, in fact
    because they are currently absent it has to write those zeroes
    itself.
    
    So we shouldn't be stripping leading zeroes in the first place.
    In fact this patch makes rsa-generic produce output with fixed
    length so that pkcs1pad does not need to do any extra work.
    
    This patch also changes DH to use the new interface.
    
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>
    (cherry picked from commit 9b45b7bba3d22de52e09df63c50f390a193a3f53)
    
    This cherry-pick, together with several supporting patches is necessary
    to maintain forward compatibility with other RSA implementations like
    NXP-SEC or software RSA in kernel versions > 4.7.
    
    Signed-off-by: Cristian Stoica <cristian.stoica@nxp.com>

--- crypto/rsa.c
+++ crypto/rsa.c
@@ -107,7 +107,7 @@ static int rsa_enc(struct akcipher_request *req)
 	if (ret)
 		goto err_free_m;
 
-	ret = mpi_write_to_sgl(c, req->dst, &req->dst_len, &sign);
+	ret = mpi_write_to_sgl(c, req->dst, req->dst_len, &sign);
 	if (ret)
 		goto err_free_m;
 
@@ -146,7 +146,7 @@ static int rsa_dec(struct akcipher_request *req)
 	if (ret)
 		goto err_free_c;
 
-	ret = mpi_write_to_sgl(m, req->dst, &req->dst_len, &sign);
+	ret = mpi_write_to_sgl(m, req->dst, req->dst_len, &sign);
 	if (ret)
 		goto err_free_c;
 
@@ -184,7 +184,7 @@ static int rsa_sign(struct akcipher_request *req)
 	if (ret)
 		goto err_free_m;
 
-	ret = mpi_write_to_sgl(s, req->dst, &req->dst_len, &sign);
+	ret = mpi_write_to_sgl(s, req->dst, req->dst_len, &sign);
 	if (ret)
 		goto err_free_m;
 
@@ -225,7 +225,7 @@ static int rsa_verify(struct akcipher_request *req)
 	if (ret)
 		goto err_free_s;
 
-	ret = mpi_write_to_sgl(m, req->dst, &req->dst_len, &sign);
+	ret = mpi_write_to_sgl(m, req->dst, req->dst_len, &sign);
 	if (ret)
 		goto err_free_s;
 

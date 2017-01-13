65966e323339ed9dc25171726fc250a83a2ceec3
Yuan Yao <yao.yuan@nxp.com>
2017-01-13 15:49:53 +0800

    mtd: spi-nor: fsl-quad: allow sub node probe failed
    The QSPI can support max to four spi flash at the same time.
    But sometime one or more flash maybe probe failed because of the
    wrong setting or the hardware issue or some othere.
    
    But as long as there is one flash probe success, that the QSPI driver
    should probe success and finish all the initialization.
    
    Signed-off-by: Yuan Yao <yao.yuan@nxp.com>

--- drivers/mtd/spi-nor/fsl-quadspi.c
+++ drivers/mtd/spi-nor/fsl-quadspi.c
@@ -1166,6 +1166,7 @@ static int fsl_qspi_probe(struct platform_device *pdev)
 	struct spi_nor *nor;
 	struct mtd_info *mtd;
 	int ret, i = 0;
+	int find_node;
 	enum read_mode mode = SPI_NOR_QUAD;
 
 	q = devm_kzalloc(dev, sizeof(*q), GFP_KERNEL);
@@ -1243,6 +1244,7 @@ static int fsl_qspi_probe(struct platform_device *pdev)
 
 	mutex_init(&q->lock);
 
+	find_node = 0;
 	/* iterate the subnodes. */
 	for_each_available_child_of_node(dev->of_node, np) {
 		/* skip the holes */
@@ -1269,7 +1271,7 @@ static int fsl_qspi_probe(struct platform_device *pdev)
 		ret = of_property_read_u32(np, "spi-max-frequency",
 				&q->clk_rate);
 		if (ret < 0)
-			goto mutex_failed;
+			continue;
 
 		/* set the chip address for READID */
 		fsl_qspi_set_base_addr(q, nor);
@@ -1283,11 +1285,11 @@ static int fsl_qspi_probe(struct platform_device *pdev)
 
 		ret = spi_nor_scan(nor, NULL, mode);
 		if (ret)
-			goto mutex_failed;
+			continue;
 
 		ret = mtd_device_register(mtd, NULL, 0);
 		if (ret)
-			goto mutex_failed;
+			continue;
 
 		/* Set the correct NOR size now. */
 		if (q->nor_size == 0) {
@@ -1310,7 +1312,11 @@ static int fsl_qspi_probe(struct platform_device *pdev)
 			nor->page_size = q->devtype_data->txfifo;
 
 		i++;
+		find_node++;
 	}
+
+	if (find_node == 0)
+		goto mutex_failed;
 
 	/* finish the rest init. */
 	ret = fsl_qspi_nor_setup_last(q);

f8deab20de515f8936ee2f60d95ad2301c4a991a
yinbo.zhu <yinbo.zhu@nxp.com>
2017-04-23 19:14:43 +0800

    USB3: DWC3: fix usb cause crash issue
    I refer linux-sdk2.0 drivers/usb/dwc3/core.c
    
    Signed-off-by: yinbo.zhu <yinbo.zhu@nxp.com>

--- drivers/usb/dwc3/core.c
+++ drivers/usb/dwc3/core.c
@@ -144,32 +144,6 @@ static int dwc3_soft_reset(struct dwc3 *dwc)
 	return 0;
 }
 
-/*
- * dwc3_frame_length_adjustment - Adjusts frame length if required
- * @dwc3: Pointer to our controller context structure
- * @fladj: Value of GFLADJ_30MHZ to adjust frame length
- */
-static void dwc3_frame_length_adjustment(struct dwc3 *dwc, u32 fladj)
-{
-	u32 reg;
-	u32 dft;
-
-	if (dwc->revision < DWC3_REVISION_250A)
-		return;
-
-	if (fladj == 0)
-		return;
-
-	reg = dwc3_readl(dwc->regs, DWC3_GFLADJ);
-	dft = reg & DWC3_GFLADJ_30MHZ_MASK;
-	if (!dev_WARN_ONCE(dwc->dev, dft == fladj,
-	    "request value same as default, ignoring\n")) {
-		reg &= ~DWC3_GFLADJ_30MHZ_MASK;
-		reg |= DWC3_GFLADJ_30MHZ_SDBND_SEL | fladj;
-		dwc3_writel(dwc->regs, DWC3_GFLADJ, reg);
-	}
-}
-
 /**
  * dwc3_free_one_event_buffer - Frees one event buffer
  * @dwc: Pointer to our controller context structure
@@ -1216,8 +1190,6 @@ static int dwc3_probe(struct platform_device *pdev)
 	if (dwc->configure_gfladj)
 		dwc3_writel(dwc->regs, DWC3_GFLADJ, GFLADJ_30MHZ_REG_SEL |
 			GFLADJ_30MHZ(GFLADJ_30MHZ_DEFAULT));
-
-	dwc3_frame_length_adjustment(dwc, fladj);
 
 	usb_phy_set_suspend(dwc->usb2_phy, 0);
 	usb_phy_set_suspend(dwc->usb3_phy, 0);

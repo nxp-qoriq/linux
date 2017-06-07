8ebbf120d28a81ca67def513e322ec54825dbd01
Madalin Bucur <madalin.bucur@nxp.com>
2017-06-07 17:54:10 +0300

    export arch_setup_dma_ops()
    Signed-off-by: Madalin Bucur <madalin.bucur@nxp.com>

--- arch/arm64/mm/dma-mapping.c
+++ arch/arm64/mm/dma-mapping.c
@@ -993,3 +993,4 @@ void arch_setup_dma_ops(struct device *dev, u64 dma_base, u64 size,
 	dev->archdata.dma_coherent = coherent;
 	__iommu_setup_dma_ops(dev, dma_base, size, iommu);
 }
+EXPORT_SYMBOL(arch_setup_dma_ops);

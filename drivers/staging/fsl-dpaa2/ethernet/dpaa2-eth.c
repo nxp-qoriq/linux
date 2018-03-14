2513a92b6be1666f3a813d73dc3d1d94b9b6d72a
Ioana Radulescu <ruxandra.radulescu@nxp.com>
2018-03-14 17:24:22 +0200

    staging: fsl-dpaa2/eth: Fix remove sequence
    On device remove, we delete the napi instances before calling
    unregister_netdev, which does a napi_synchronize if the interface
    is still up at this point. This may result in a kernel hang.
    
    Fix this by deleting the napi instances after unregistering the
    netdevice.
    
    Signed-off-by: Ioana Radulescu <ruxandra.radulescu@nxp.com>

--- drivers/staging/fsl-dpaa2/ethernet/dpaa2-eth.c
+++ drivers/staging/fsl-dpaa2/ethernet/dpaa2-eth.c
@@ -3467,11 +3467,11 @@ static int dpaa2_eth_remove(struct fsl_mc_device *ls_dev)
 #endif
 	dpaa2_eth_sysfs_remove(&net_dev->dev);
 
-	disable_ch_napi(priv);
-	del_ch_napi(priv);
-
 	unregister_netdev(net_dev);
 	dev_info(net_dev->dev.parent, "Removed interface %s\n", net_dev->name);
+
+	disable_ch_napi(priv);
+	del_ch_napi(priv);
 
 	if (priv->do_link_poll)
 		kthread_stop(priv->poll_thread);

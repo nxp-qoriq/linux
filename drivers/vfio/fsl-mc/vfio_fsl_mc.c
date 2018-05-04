7c00ddac52545ee84af8be298365c60914cee08c
Bharat Bhushan <Bharat.Bhushan@nxp.com>
2018-05-04 14:36:29 +0800

    vfio fsl-mc: Fix header file reference
    Change header file reference as mc-bus driver moves
    out of staging.
    
    These changes will be squashed with other changes when
    sending upstream.
    
    Signed-off-by: Bharat Bhushan <Bharat.Bhushan@nxp.com>

--- drivers/vfio/fsl-mc/vfio_fsl_mc.c
+++ drivers/vfio/fsl-mc/vfio_fsl_mc.c
@@ -19,10 +19,7 @@
 #include <linux/vfio.h>
 #include <linux/delay.h>
 
-#include "../../staging/fsl-mc/include/mc.h"
-#include "../../staging/fsl-mc/include/mc-bus.h"
-#include "../../staging/fsl-mc/include/mc-sys.h"
-#include "../../staging/fsl-mc/bus/dprc-cmd.h"
+#include <linux/fsl/mc.h>
 
 #include "vfio_fsl_mc_private.h"
 

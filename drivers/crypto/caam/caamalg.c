88143b33be2c21b4ef72bb209fe8c1d2b77ce3ee
Horia Geantă <horia.geanta@nxp.com>
2018-05-14 18:14:43 +0300

    [tmp workaround] crypto: caam/jr - disable failing algos
    Some of the algorithms are failing on the JR interface:
    rfc7539-chacha20-poly1305-caam
    rfc7539esp-chacha20-poly1305-caam
    rfc4543-gcm-aes-caam
    
    Last two of them cause testmgr (and CAAM driver) to hang,
    waiting indefinitely after CAAM engine.
    
    As a workaround temporary disable only these two (and let the first one
    to fail during crypto self tests).
    
    Note: compiler warnings introduced by this fix are benign.
    Trying to fix warnings would be to much trouble, so just ignore them.
    
    Signed-off-by: Horia Geantă <horia.geanta@nxp.com>

--- drivers/crypto/caam/caamalg.c
+++ drivers/crypto/caam/caamalg.c
@@ -2363,6 +2363,7 @@ static struct caam_aead_alg driver_aeads[] = {
 			.class1_alg_type = OP_ALG_ALGSEL_AES | OP_ALG_AAI_GCM,
 		},
 	},
+#if 0
 	{
 		.aead = {
 			.base = {
@@ -2381,6 +2382,7 @@ static struct caam_aead_alg driver_aeads[] = {
 			.class1_alg_type = OP_ALG_ALGSEL_AES | OP_ALG_AAI_GCM,
 		},
 	},
+#endif
 	/* Galois Counter Mode */
 	{
 		.aead = {
@@ -3644,6 +3646,7 @@ static struct caam_aead_alg driver_aeads[] = {
 					   OP_ALG_AAI_AEAD,
 		},
 	},
+#if 0
 	{
 		.aead = {
 			.base = {
@@ -3665,6 +3668,7 @@ static struct caam_aead_alg driver_aeads[] = {
 					   OP_ALG_AAI_AEAD,
 		},
 	},
+#endif
 };
 
 struct caam_crypto_alg {

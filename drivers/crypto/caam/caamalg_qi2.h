efd91395d8f360b5fd2b987afd2aeb87086bd488
Horia Geantă <horia.geanta@nxp.com>
2018-07-17 15:14:43 +0300

    crypto: caam/qi2 - ablkcipher -> skcipher conversion
    Before upstreaming the driver, convert it from deprecated ablkcipher API
    to skcipher.
    
    Link: https://www.mail-archive.com/search?l=mid&q=20170728085622.GC19664@gondor.apana.org.au
    Signed-off-by: Horia Geantă <horia.geanta@nxp.com>

--- drivers/crypto/caam/caamalg_qi2.h
+++ drivers/crypto/caam/caamalg_qi2.h
@@ -192,7 +192,7 @@ struct tls_edesc {
 };
 
 /*
- * ablkcipher_edesc - s/w-extended ablkcipher descriptor
+ * skcipher_edesc - s/w-extended skcipher descriptor
  * @src_nents: number of segments in input scatterlist
  * @dst_nents: number of segments in output scatterlist
  * @iv_dma: dma address of iv for checking continuity and link table
@@ -200,7 +200,7 @@ struct tls_edesc {
  * @qm_sg_dma: I/O virtual address of h/w link table
  * @sgt: the h/w link table, followed by IV
  */
-struct ablkcipher_edesc {
+struct skcipher_edesc {
 	int src_nents;
 	int dst_nents;
 	dma_addr_t iv_dma;
@@ -253,7 +253,7 @@ enum optype {
  * @op_type: operation type
  * @cbk: Callback function to invoke when job is completed
  * @ctx: arbit context attached with request by the application
- * @edesc: extended descriptor; points to one of {ablkcipher,aead}_edesc
+ * @edesc: extended descriptor; points to one of {skcipher,aead}_edesc
  */
 struct caam_request {
 	struct dpaa2_fl_entry fd_flt[2];

d67d37e404ce6b50a10b7c205444f5fbcf6e0278
Valentin Catalin Neacsu <valentin-catalin.neacsu@nxp.com>
2018-08-06 14:24:59 +0300

    USXGMII suport for aquantia phy

--- drivers/staging/fsl-dpaa2/mac/mac.c
+++ drivers/staging/fsl-dpaa2/mac/mac.c
@@ -78,6 +78,9 @@ static phy_interface_t dpaa2_mac_iface_mode[] =  {
 	PHY_INTERFACE_MODE_XGMII,	/* DPMAC_ETH_IF_XAUI */
 	PHY_INTERFACE_MODE_XGMII,	/* DPMAC_ETH_IF_XFI */
 	PHY_INTERFACE_MODE_CAUI,	/* DPMAC_ETH_IF_CAUI */
+	PHY_INTERFACE_MODE_XGMII,	/* DPMAC_ETH_IF_1000BASEX */
+	PHY_INTERFACE_MODE_XGMII,	/* DPMAC_ETH_IF_USXGMII */
+
 };
 
 static void dpaa2_mac_link_changed(struct net_device *netdev)

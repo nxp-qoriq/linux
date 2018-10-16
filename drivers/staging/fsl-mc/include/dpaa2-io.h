c339753a42db88de9faccf03188233b935148e3e
Ioana Ciornei <ioana.ciornei@nxp.com>
2018-10-16 17:33:16 +0300

    soc: fsl: dpio: identify the cpu bound to a dpio_io
    Signed-off-by: Ioana Ciornei <ioana.ciornei@nxp.com>

--- drivers/staging/fsl-mc/include/dpaa2-io.h
+++ drivers/staging/fsl-mc/include/dpaa2-io.h
@@ -91,6 +91,8 @@ struct dpaa2_io_notification_ctx {
 	void *dpio_private;
 };
 
+int dpaa2_io_get_cpu(struct dpaa2_io *d);
+
 int dpaa2_io_service_register(struct dpaa2_io *service,
 			      struct dpaa2_io_notification_ctx *ctx,
 			      struct device *dev);

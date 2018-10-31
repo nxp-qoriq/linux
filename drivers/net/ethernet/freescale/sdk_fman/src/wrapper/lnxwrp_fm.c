1818e9e58c126424c3e13ba1fa97b13063189265
Yangbo Lu <yangbo.lu@nxp.com>
2018-10-31 17:07:40 +0800

    sdk_fman: disable ptp timer probe
    DPAA PTP timer was managed by ptp_qoriq driver in drivers/ptp/.
    We will no longer manage it in sdk_fman driver and use related
    APIs.
    
    Signed-off-by: Yangbo Lu <yangbo.lu@nxp.com>

--- drivers/net/ethernet/freescale/sdk_fman/src/wrapper/lnxwrp_fm.c
+++ drivers/net/ethernet/freescale/sdk_fman/src/wrapper/lnxwrp_fm.c
@@ -682,6 +682,7 @@ static t_LnxWrpFmDev * ReadFmDevTreeNode (struct platform_device *of_dev)
         }
     }
 
+#if 0
     /* Get the RTC base address and size */
     memset(ids, 0, sizeof(ids));
     if (WARN_ON(strlen("ptp-timer") >= sizeof(ids[0].name)))
@@ -704,6 +705,7 @@ static t_LnxWrpFmDev * ReadFmDevTreeNode (struct platform_device *of_dev)
             p_LnxWrpFmDev->fmRtcMemSize = res.end + 1 - res.start;
         }
     }
+#endif
 
 #if (DPAA_VERSION >= 11)
     /* Get the VSP base address */

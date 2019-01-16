9e4006d68fb1a4e6fe3083892f83b0d0f4c3a3cd
Ran Wang <ran.wang_1@nxp.com>
2019-01-16 13:23:17 +0800

    usb: dwc3: Add workaround for host mode VBUS glitch when boot
    When DWC3 is set to host mode by programming register DWC3_GCTL, VBUS
    (or its control signal) will be turned on immediately on related Root Hub
    ports. Then, the VBUS is turned off for a little while(15us) when do xhci
    reset (conducted by xhci driver) and back to normal finally, we can
    observe a negative glitch of related signal happen.
    
    This VBUS glitch might cause some USB devices enumeration fail if kernel
    boot with them connected. Such as LS1012AFWRY/LS1043ARDB/LX2160AQDS
    /LS1088ARDB with Kingston 16GB USB2.0/Kingston USB3.0/JetFlash Transcend
    4GB USB2.0 drives. The fail cases include enumerated as full-speed device
    or report wrong device descriptor, etc.
    
    One SW workaround which can fix this is by programing all xhci PORTSC[PP]
    to 0 to turn off VBUS immediately after setting host mode in DWC3 driver
    (per signal measurement result, it will be too late to do it in
    xhci-plat.c or xhci.c). Then, after xhci reset complete in xhci driver,
    PORTSC[PP]s' value will back to 1 automatically and VBUS on at that time,
    no glitch happen and normal enumeration process has no impact.
    
    Signed-off-by: Ran Wang <ran.wang_1@nxp.com>
    Reviewed-by: Peter Chen <peter.chen@nxp.com>

--- drivers/usb/dwc3/core.c
+++ drivers/usb/dwc3/core.c
@@ -1233,6 +1233,8 @@ static void dwc3_get_properties(struct dwc3 *dwc)
 				    &dwc->hsphy_interface);
 	device_property_read_u32(dev, "snps,quirk-frame-length-adjustment",
 				 &dwc->fladj);
+	dwc->host_vbus_glitches = device_property_read_bool(dev,
+				"snps,host-vbus-glitches");
 
 	dwc->lpm_nyet_threshold = lpm_nyet_threshold;
 	dwc->tx_de_emphasis = tx_de_emphasis;

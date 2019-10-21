def1def5381a8415aec0c46d5b49d0faefa0298d
Vivien Didelot <vivien.didelot@gmail.com>
2019-10-21 16:51:30 -0400

    net: dsa: remove dsa_switch_alloc helper
    Now that ports are dynamically listed in the fabric, there is no need
    to provide a special helper to allocate the dsa_switch structure. This
    will give more flexibility to drivers to embed this structure as they
    wish in their private structure.
    
    Signed-off-by: Vivien Didelot <vivien.didelot@gmail.com>
    Reviewed-by: Florian Fainelli <f.fainelli@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>

--- drivers/net/dsa/ocelot/felix.c
+++ drivers/net/dsa/ocelot/felix.c
@@ -846,7 +846,7 @@ static int felix_pci_probe(struct pci_dev *pdev,
 
 	ocelot->ptp = 1;
 
-	ds = dsa_switch_alloc(&pdev->dev, felix->info->num_ports);
+	ds = kzalloc(sizeof(struct dsa_switch), GFP_KERNEL);
 	if (!ds) {
 		err = -ENOMEM;
 		dev_err(&pdev->dev, "Failed to allocate DSA switch\n");

f3568890952cbb2a95ca9afa26b0f88bb79a18e3
Nikhil Gupta <nikhil.gupta@nxp.com>
2019-12-30 15:11:23 +0530

    arm64:move elfcorehdr reservation early for crash dump kernel
    on some SOCs, elfcorehdr address may overlap with the address of reserved
    memory allocated using early_init_fdt_scan_reserved_mem
    
    Signed-off-by: Nikhil Gupta <nikhil.gupta@nxp.com>
    Signed-off-by: Poonam Aggrwal <poonam.aggrwal@nxp.com>

--- arch/arm64/mm/init.c
+++ arch/arm64/mm/init.c
@@ -472,6 +472,8 @@ void __init arm64_memblock_init(void)
 	}
 #endif
 
+	reserve_elfcorehdr();
+
 	early_init_fdt_scan_reserved_mem();
 
 	/* 4GB maximum for 32-bit only capable devices */
@@ -481,8 +483,6 @@ void __init arm64_memblock_init(void)
 		arm64_dma_phys_limit = PHYS_MASK + 1;
 
 	reserve_crashkernel();
-
-	reserve_elfcorehdr();
 
 	high_memory = __va(memblock_end_of_DRAM() - 1) + 1;
 

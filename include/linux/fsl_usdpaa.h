e518e2480508d1fa75674f35376ad9a8155cdc44
Sachin Saxena <sachin.saxena@oss.nxp.com>
2020-02-11 16:05:13 +0530

    fsl_qbman: Framework for enabling Link status notification and updating link status
    This will enable link update event notification and link up/down
    APIs for user space USDPAA application.
    
    Signed-off-by: Rohit Raj <rohit.raj@nxp.com>
    Signed-off-by: Sachin Saxena <sachin.saxena@nxp.com>
    DPDK-2191

--- include/linux/fsl_usdpaa.h
+++ include/linux/fsl_usdpaa.h
@@ -365,6 +365,49 @@ int dpa_alloc_pop(struct dpa_alloc *alloc, u32 *result, u32 *count);
 int dpa_alloc_check(struct dpa_alloc *list, u32 id);
 #endif /* __KERNEL__ */
 
+
+/************************************
+ * Link Status support for user space
+ * interface
+ ************************************/
+#define IF_NAME_MAX_LEN 16
+#define NODE_NAME_LEN	32
+#define ETH_LINK_DOWN 0
+#define ETH_LINK_UP 1
+
+struct usdpaa_ioctl_link_status {
+	/* network device node name */
+	char		if_name[IF_NAME_MAX_LEN];
+	/* Eventfd value */
+	uint32_t	efd;
+};
+
+#define USDPAA_IOCTL_ENABLE_LINK_STATUS_INTERRUPT \
+	_IOW(USDPAA_IOCTL_MAGIC, 0x0E, struct usdpaa_ioctl_link_status)
+
+#define USDPAA_IOCTL_DISABLE_LINK_STATUS_INTERRUPT \
+	_IOW(USDPAA_IOCTL_MAGIC, 0x0F, char *)
+
+struct usdpaa_ioctl_link_status_args {
+	/* network device node name */
+	char    if_name[IF_NAME_MAX_LEN];
+	/* link status(UP/DOWN) */
+	int     link_status;
+};
+
+struct usdpaa_ioctl_update_link_status {
+	/* network device node name */
+	char    if_name[IF_NAME_MAX_LEN];
+	/* link status(ETH_LINK_UP/DOWN) */
+	int     set_link_status;
+};
+
+#define USDPAA_IOCTL_GET_LINK_STATUS \
+	_IOWR(USDPAA_IOCTL_MAGIC, 0x10, struct usdpaa_ioctl_link_status_args)
+
+#define USDPAA_IOCTL_UPDATE_LINK_STATUS \
+	_IOW(USDPAA_IOCTL_MAGIC, 0x11, struct usdpaa_ioctl_update_link_status)
+
 #ifdef __cplusplus
 }
 #endif

13afd0d0a2a82d85f75f796c9159993390ee3d69
Laxminarayan Mishra <laxminarayan.mishra@nxp.com>
2020-02-14 17:53:55 +0000

    LA1246-RDB: Fix NXP licensing

--- drivers/net/ethernet/freescale/sdk_dpaa/dpaa_eth_base.c
+++ drivers/net/ethernet/freescale/sdk_dpaa/dpaa_eth_base.c
@@ -1,5 +1,5 @@
 /* Copyright 2008-2013 Freescale Semiconductor, Inc.
- *
+ * Copyright 2020 NXP
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
  *     * Redistributions of source code must retain the above copyright
--- include/linux/fsl_usdpaa.h
+++ include/linux/fsl_usdpaa.h
@@ -1,5 +1,5 @@
 /* Copyright 2011-2012 Freescale Semiconductor, Inc.
- *
+ * Copyright 2020 NXP
  * This file is licensed under the terms of the GNU General Public License
  * version 2.  This program is licensed "as is" without any warranty of any
  * kind, whether express or implied.

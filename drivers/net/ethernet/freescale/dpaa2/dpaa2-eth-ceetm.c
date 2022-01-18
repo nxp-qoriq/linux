b2efa9ad854f33d7c5f89f0038d619dd3e2cc74a
Jiafei Pan <Jiafei.Pan@nxp.com>
2022-01-18 12:29:04 +0800

    net: dpaa2-ceetm: sync to bstats new definition
    Packed non-per-CPU bstats data type "struct gnet_stats_basic_packed"
    is replaced by "struct gnet_stats_basic_sync", this patch is to
     make the driver to be compatible with new bstats definitions.
    
    Signed-off-by: Jiafei Pan <Jiafei.Pan@nxp.com>

--- drivers/net/ethernet/freescale/dpaa2/dpaa2-eth-ceetm.c
+++ drivers/net/ethernet/freescale/dpaa2/dpaa2-eth-ceetm.c
@@ -371,14 +371,16 @@ static int dpaa2_ceetm_dump(struct Qdisc *sch, struct sk_buff *skb)
 	case CEETM_ROOT:
 		/* Gather statistics from the underlying pfifo qdiscs */
 		sch->q.qlen = 0;
-		memset(&sch->bstats, 0, sizeof(sch->bstats));
+		gnet_stats_basic_sync_init(&sch->bstats);
 		memset(&sch->qstats, 0, sizeof(sch->qstats));
 
 		for (ntx = 0; ntx < dev->num_tx_queues; ntx++) {
 			qdisc = netdev_get_tx_queue(dev, ntx)->qdisc_sleeping;
 			sch->q.qlen		+= qdisc->q.qlen;
-			sch->bstats.bytes	+= qdisc->bstats.bytes;
-			sch->bstats.packets	+= qdisc->bstats.packets;
+			u64_stats_add(&sch->bstats.bytes,
+					u64_stats_read(&qdisc->bstats.bytes));
+			u64_stats_add(&sch->bstats.packets,
+					u64_stats_read(&qdisc->bstats.packets));
 			sch->qstats.qlen	+= qdisc->qstats.qlen;
 			sch->qstats.backlog	+= qdisc->qstats.backlog;
 			sch->qstats.drops	+= qdisc->qstats.drops;
@@ -743,6 +745,7 @@ static int dpaa2_ceetm_cls_change_prio(struct dpaa2_ceetm_class *cl,
 			       __func__);
 			return -ENOMEM;
 		}
+		gnet_stats_basic_sync_init(&cl->prio.cstats->bstats);
 	}
 
 	cl->prio.mode = copt->mode;
@@ -1051,7 +1054,7 @@ static int dpaa2_ceetm_cls_dump_stats(struct Qdisc *sch, unsigned long arg,
 				      struct gnet_dump *d)
 {
 	struct dpaa2_ceetm_class *cl = (struct dpaa2_ceetm_class *)arg;
-	struct gnet_stats_basic_packed tmp_bstats;
+	struct gnet_stats_basic_sync tmp_bstats;
 	struct dpaa2_ceetm_tc_xstats xstats;
 	union dpni_statistics dpni_stats;
 	struct net_device *dev = qdisc_dev(sch);
@@ -1060,7 +1063,7 @@ static int dpaa2_ceetm_cls_dump_stats(struct Qdisc *sch, unsigned long arg,
 	int err;
 
 	memset(&xstats, 0, sizeof(xstats));
-	memset(&tmp_bstats, 0, sizeof(tmp_bstats));
+	gnet_stats_basic_sync_init(&tmp_bstats);
 
 	if (cl->type == CEETM_ROOT)
 		return 0;

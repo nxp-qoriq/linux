5beefb9066fc62441f65661ee6ef23c3e325bb14
Marouen Ghodhbane <marouen.ghodhbane@nxp.com>
2023-01-04 18:03:15 +0100

    clk: imx-pll: add suport for the fracn gppll
    Signed-off-by: Marouen Ghodhbane <marouen.ghodhbane@nxp.com>

--- drivers/clk/imx/clk-fracn-gppll.c
+++ drivers/clk/imx/clk-fracn-gppll.c
@@ -13,6 +13,7 @@
 #include <asm/div64.h>
 
 #include "clk.h"
+#include "clk-pll.h"
 
 #define PLL_CTRL		0x0
 #define CLKMUX_BYPASS		BIT(2)
@@ -52,10 +53,21 @@
 		.odiv	=	(_odiv),			\
 	}
 
+
+/* MFN : Numerator of the fractional part of divider (30-bits, signed) */
+#define MFN_MAX_VALUE			((s32)(GENMASK(29, 0) >> 1))
+#define MFN_MIN_VALUE			((s32)(-MFN_MAX_VALUE - 1))
+
 struct clk_fracn_gppll {
 	struct clk_hw			hw;
 	void __iomem			*base;
 	const struct imx_fracn_gppll_rate_table *rate_table;
+	u32				orig_mfd;
+	u32				orig_mfi;
+	u32				orig_odiv;
+	u32				orig_rdiv;
+	s32				orig_mfn;
+	struct clk_imx_pll		imx_pll;
 	int rate_count;
 };
 
@@ -115,13 +127,55 @@ static long clk_fracn_gppll_round_rate(struct clk_hw *hw, unsigned long rate,
 	return rate_table[pll->rate_count - 1].rate;
 }
 
+/* This function calculates the actual rate based on the configured PLL registers */
+static unsigned long __clk_fracn_gppll_recalc_rate(struct clk_hw *hw, unsigned long parent_rate)
+{
+	struct clk_fracn_gppll *pll = to_clk_fracn_gppll(hw);
+	u32 pll_numerator, pll_denominator, pll_div;
+	u32 mfi, mfd, rdiv, odiv;
+	s32 mfn;
+	u64 fvco = parent_rate;
+
+	pll_numerator = readl_relaxed(pll->base + PLL_NUMERATOR);
+
+	/* Numerator is 30-bits signed value */
+	mfn = sign_extend32(FIELD_GET(PLL_MFN_MASK, pll_numerator), 29);
+
+	pll_denominator = readl_relaxed(pll->base + PLL_DENOMINATOR);
+	mfd = FIELD_GET(PLL_MFD_MASK, pll_denominator);
+
+	pll_div = readl_relaxed(pll->base + PLL_DIV);
+	mfi = FIELD_GET(PLL_MFI_MASK, pll_div);
+
+	rdiv = FIELD_GET(PLL_RDIV_MASK, pll_div);
+	odiv = FIELD_GET(PLL_ODIV_MASK, pll_div);
+
+	rdiv = rdiv + 1;
+
+	switch (odiv) {
+	case 0:
+		odiv = 2;
+		break;
+	case 1:
+		odiv = 3;
+		break;
+	default:
+		break;
+	}
+
+	/* Fvco = Fref * (MFI + MFN / MFD) */
+	fvco *= (mfi * mfd + mfn);
+	do_div(fvco, mfd * rdiv * odiv);
+
+	return (unsigned long)fvco;
+}
+
 static unsigned long clk_fracn_gppll_recalc_rate(struct clk_hw *hw, unsigned long parent_rate)
 {
 	struct clk_fracn_gppll *pll = to_clk_fracn_gppll(hw);
 	const struct imx_fracn_gppll_rate_table *rate_table = pll->rate_table;
 	u32 pll_numerator, pll_denominator, pll_div;
 	u32 mfi, mfn, mfd, rdiv, odiv;
-	u64 fvco = parent_rate;
 	long rate = 0;
 	int i;
 
@@ -150,27 +204,7 @@ static unsigned long clk_fracn_gppll_recalc_rate(struct clk_hw *hw, unsigned lon
 			rate = rate_table[i].rate;
 	}
 
-	if (rate)
-		return (unsigned long)rate;
-
-	rdiv = rdiv + 1;
-
-	switch (odiv) {
-	case 0:
-		odiv = 2;
-		break;
-	case 1:
-		odiv = 3;
-		break;
-	default:
-		break;
-	}
-
-	/* Fvco = Fref * (MFI + MFN / MFD) */
-	fvco = fvco * mfi * mfd + fvco * mfn;
-	do_div(fvco, mfd * rdiv * odiv);
-
-	return (unsigned long)fvco;
+	return rate ? (unsigned long) rate : __clk_fracn_gppll_recalc_rate(hw, parent_rate);
 }
 
 static int clk_fracn_gppll_wait_lock(struct clk_fracn_gppll *pll)
@@ -190,6 +224,11 @@ static int clk_fracn_gppll_set_rate(struct clk_hw *hw, unsigned long drate,
 	int ret;
 
 	rate = imx_get_pll_settings(pll, drate);
+	if (!rate) {
+		pr_err("%s: Invalid rate : %lu for pll clk %s\n", __func__,
+			drate, clk_hw_get_name(hw));
+		return -EINVAL;
+	}
 
 	/* Disable output */
 	tmp = readl_relaxed(pll->base + PLL_CTRL);
@@ -292,6 +331,122 @@ static const struct clk_ops clk_fracn_gppll_ops = {
 	.set_rate	= clk_fracn_gppll_set_rate,
 };
 
+/* This function fetches the original PLL parameters to use
+ * them later for ppb adjustment
+ */
+static void imx_fracn_gppll_init(struct clk_imx_pll *pll)
+{
+	struct clk_fracn_gppll *fracn_pll;
+	u32 pll_numerator, pll_denominator, pll_div, mfn;
+
+	fracn_pll = (struct clk_fracn_gppll *) container_of(pll,
+					struct clk_fracn_gppll, imx_pll);
+
+	pll_numerator = readl_relaxed(fracn_pll->base + PLL_NUMERATOR);
+	mfn = FIELD_GET(PLL_MFN_MASK, pll_numerator);
+
+	/* Numerator is 30-bits signed value */
+	fracn_pll->orig_mfn = sign_extend32(mfn, 29);
+
+	pll_denominator = readl_relaxed(fracn_pll->base + PLL_DENOMINATOR);
+	fracn_pll->orig_mfd = FIELD_GET(PLL_MFD_MASK, pll_denominator);
+
+	pll_div = readl_relaxed(fracn_pll->base + PLL_DIV);
+	fracn_pll->orig_mfi = FIELD_GET(PLL_MFI_MASK, pll_div);
+
+	fracn_pll->orig_rdiv = FIELD_GET(PLL_RDIV_MASK, pll_div);
+	fracn_pll->orig_odiv = FIELD_GET(PLL_ODIV_MASK, pll_div);
+}
+
+/**
+ * imx_fracn_gppll_adjust - Adjust the Audio pll by ppb.
+ *
+ * This function adjusts the audio pll by ppb (part per billion) and returns
+ * the exact number of ppb adjusted.
+ * The adjustment is done by only modifying the numerator of the fractional part
+ * of the audio PLL.
+ * Since the pllout = (ref * (mfi + mfn/mfd)) / (rdiv * odiv)
+ * and the adjusted value is
+ *    pllout_new = pllout * (1 + ppb/1e9) which equals:
+ *    (ref * (mfi + mfn_new/mfd)) / (rdiv * odiv)
+ * The new numerator (mfn_new) is calculated as following:
+ *    mfn_new = (1e9 * mfn + (mfi * mfd + mfn) * ppb) / (1e9)
+ */
+
+static int imx_fracn_gppll_adjust(struct clk_imx_pll *pll, int *ppb)
+{
+	s64 temp64;
+	s64 applied_ppb;
+	struct clk_fracn_gppll *fracn_pll;
+	int rc = IMX_CLK_PLL_SUCCESS;
+
+	int req_ppb = *ppb;
+
+	fracn_pll = (struct clk_fracn_gppll *) container_of(pll,
+						struct clk_fracn_gppll, imx_pll);
+
+	/* Calculate the new numerator value */
+	temp64 = ((s64) fracn_pll->orig_mfn * 1000000000)
+			+ ((s64) fracn_pll->orig_mfi * fracn_pll->orig_mfd + (s64) fracn_pll->orig_mfn) * req_ppb;
+
+	temp64 = div_s64(temp64, 1000000000);
+
+	/* Sanity check on the new numerator value:
+	 * 	- the value is inside the 30-bits signed values range
+	 *	- mfn/mfd should be in the range [-2, 2]
+	 */
+	if (temp64 > MFN_MAX_VALUE || temp64 < MFN_MIN_VALUE || (abs(temp64) > (2 * fracn_pll->orig_mfd))) {
+		rc = -IMX_CLK_PLL_PREC_ERR;
+		goto exit;
+	}
+
+	/* Write the PLL control settings with the new numerator */
+
+	writel_relaxed(FIELD_PREP(PLL_MFN_MASK, temp64), fracn_pll->base + PLL_NUMERATOR);
+
+	/* Calculate and return the actual applied ppb */
+	applied_ppb = div64_s64((s64) (temp64 - fracn_pll->orig_mfn) * 1000000000,
+			fracn_pll->orig_mfn + fracn_pll->orig_mfd * (s64) fracn_pll->orig_mfi);
+
+	*ppb = (int) applied_ppb;
+
+ exit:
+	return rc;
+}
+
+static unsigned long imx_fracn_gppll_get_rate(struct clk_imx_pll *pll,
+					   unsigned long parent_rate)
+{
+	struct clk_fracn_gppll *fracn_pll;
+
+	fracn_pll = (struct clk_fracn_gppll *) container_of(pll,
+						struct clk_fracn_gppll, imx_pll);
+
+	return __clk_fracn_gppll_recalc_rate(&fracn_pll->hw, parent_rate);
+}
+
+static int imx_fracn_gppll_set_rate(struct clk_imx_pll *pll, unsigned long rate,
+				 unsigned long parent_rate)
+{
+	struct clk_fracn_gppll *fracn_pll;
+	int rc = IMX_CLK_PLL_SUCCESS;
+
+	fracn_pll = (struct clk_fracn_gppll *) container_of(pll,
+						struct clk_fracn_gppll, imx_pll);
+
+	if (clk_fracn_gppll_set_rate(&fracn_pll->hw, rate, parent_rate) < 0)
+		rc = -IMX_CLK_PLL_INVALID_PARAM;
+
+	return rc;
+}
+
+static const struct clk_imx_pll_ops imx_clk_fracn_gppll_ops = {
+	.set_rate	= imx_fracn_gppll_set_rate,
+	.get_rate	= imx_fracn_gppll_get_rate,
+	.adjust		= imx_fracn_gppll_adjust,
+	.init		= imx_fracn_gppll_init,
+};
+
 struct clk_hw *imx_clk_fracn_gppll(const char *name, const char *parent_name, void __iomem *base,
 				   const struct imx_fracn_gppll_clk *pll_clk)
 {
@@ -323,6 +478,11 @@ struct clk_hw *imx_clk_fracn_gppll(const char *name, const char *parent_name, vo
 		kfree(pll);
 		return ERR_PTR(ret);
 	}
+
+	pll->imx_pll.ops = &imx_clk_fracn_gppll_ops;
+
+	if (imx_pll_register(&pll->imx_pll, name) < 0)
+		pr_warn("%s: failed to register %s into imx pll\n", __func__, name);
 
 	return hw;
 }

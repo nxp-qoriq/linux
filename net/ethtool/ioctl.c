c51b7f0b68d2482bf68aba7ea97a38e7f17b1c69
Xiaoliang Yang <xiaoliang.yang_1@nxp.com>
2023-04-24 14:55:36 +0800

    net: ethtool: preemption: add preemption reset function
    The preemption should not be active until the lldp verify ensure that
    preemption is enabled on remote port. That needs a reset function to
    reset the preemption to make the preemption active. The user space
    application will use this API.
    
    Signed-off-by: Xiaoliang Yang <xiaoliang.yang_1@nxp.com>
    Signed-off-by: Hou Zhiqiang <Zhiqiang.Hou@nxp.com>

--- net/ethtool/ioctl.c
+++ net/ethtool/ioctl.c
@@ -3091,6 +3091,19 @@ static int ethtool_set_preempt(struct net_device *dev, void __user *useraddr)
 	return dev->ethtool_ops->set_preempt(dev, &fpparam);
 }
 
+static int ethtool_reset_preempt(struct net_device *dev, void __user *useraddr)
+{
+	struct ethtool_fp fpparam;
+
+	if (!dev->ethtool_ops->reset_preempt)
+		return -EOPNOTSUPP;
+
+	if (copy_from_user(&fpparam, useraddr, sizeof(fpparam)))
+		return -EFAULT;
+
+	return dev->ethtool_ops->reset_preempt(dev, fpparam.fp_enabled);
+}
+
 /* The main entry point in this file.  Called from net/core/dev_ioctl.c */
 
 static int
@@ -3152,6 +3165,7 @@ __dev_ethtool(struct net *net, struct ifreq *ifr, void __user *useraddr,
 	case ETHTOOL_GFECPARAM:
 	case ETHTOOL_GFP:
 	case ETHTOOL_SFP:
+	case ETHTOOL_RFP:
 		break;
 	default:
 		if (!ns_capable(net->user_ns, CAP_NET_ADMIN))
@@ -3384,6 +3398,9 @@ __dev_ethtool(struct net *net, struct ifreq *ifr, void __user *useraddr,
 		break;
 	case ETHTOOL_SFP:
 		rc = ethtool_set_preempt(dev, useraddr);
+		break;
+	case ETHTOOL_RFP:
+		rc = ethtool_reset_preempt(dev, useraddr);
 		break;
 	default:
 		rc = -EOPNOTSUPP;

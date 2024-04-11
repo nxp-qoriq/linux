09a9df9890284bd58fd8994d80f8e3359f04aadb
Xiaoliang Yang <xiaoliang.yang_1@nxp.com>
2024-04-11 17:56:43 +0800

    net: ocelot: fix udp fragment cannot being forwarded
    The ptp driver add the vcap to filter dport=319/320 UDP frames to trap
    to CPU. The fragment UDP frames without UDP header will also match the
    rule and can't be forwarded. This patch add the key to check if frame is
    fragment frame.
    
    Signed-off-by: Xiaoliang Yang <xiaoliang.yang_1@nxp.com>
    Signed-off-by: Hou Zhiqiang <Zhiqiang.Hou@nxp.com>

--- drivers/net/ethernet/mscc/ocelot_ptp.c
+++ drivers/net/ethernet/mscc/ocelot_ptp.c
@@ -327,6 +327,7 @@ static void
 ocelot_populate_ipv4_ptp_event_trap_key(struct ocelot_vcap_filter *trap)
 {
 	trap->key_type = OCELOT_VCAP_KEY_IPV4;
+	trap->key.ipv4.fragment = OCELOT_VCAP_BIT_0;
 	trap->key.ipv4.proto.value[0] = IPPROTO_UDP;
 	trap->key.ipv4.proto.mask[0] = 0xff;
 	trap->key.ipv4.dport.value = PTP_EV_PORT;
@@ -347,6 +348,7 @@ static void
 ocelot_populate_ipv4_ptp_general_trap_key(struct ocelot_vcap_filter *trap)
 {
 	trap->key_type = OCELOT_VCAP_KEY_IPV4;
+	trap->key.ipv4.fragment = OCELOT_VCAP_BIT_0;
 	trap->key.ipv4.proto.value[0] = IPPROTO_UDP;
 	trap->key.ipv4.proto.mask[0] = 0xff;
 	trap->key.ipv4.dport.value = PTP_GEN_PORT;

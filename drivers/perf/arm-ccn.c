1e013d20f04ec2f5c05a3a7d8487e0a13de20e5b
Howard Lu <howard.lu@nxp.com>
2025-10-15 12:16:54 +0530

    Add ARM CCN support for CCN-508
    Signed-off-by: Howard Lu <howard.lu@nxp.com>

--- drivers/perf/arm-ccn.c
+++ drivers/perf/arm-ccn.c
@@ -1517,6 +1517,7 @@ static void arm_ccn_remove(struct platform_device *pdev)
 static const struct of_device_id arm_ccn_match[] = {
 	{ .compatible = "arm,ccn-502", },
 	{ .compatible = "arm,ccn-504", },
+	{ .compatible = "arm,ccn-508", },
 	{ .compatible = "arm,ccn-512", },
 	{},
 };

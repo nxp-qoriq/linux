f05646894b7ac5d4d588e36767e232fdbc78f981
Hidayath Khan <hidayath@linux.ibm.com>
2026-07-30 14:42:27 +0200

    dibs: initialise dibs->lock in dibs_dev_alloc()
    commit c27e360545373b7aee9862a5beef3b9fb3df0c25 upstream.
    
    dibs->lock is initialised by dibs_dev_add(), but a dibs device can
    already take interrupts before that call: ism_probe() runs
    ism_dev_init(), and hence request_irq(), before it calls
    dibs_dev_add(). No client can have registered a dmb at that point, so
    no dmb interrupt can occur, but a GID event interrupt can, and
    ism_handle_irq() takes dibs->lock unconditionally on entry, before it
    inspects anything else.
    
    Initialise the lock in dibs_dev_alloc() instead, so that it is valid as
    soon as a driver can publish the device to its interrupt handler.
    
    Fixes: cc21191b584c ("dibs: Move data path to dibs layer")
    Cc: stable@vger.kernel.org
    Reviewed-by: Alexandra Winter <wintera@linux.ibm.com>
    Signed-off-by: Hidayath Khan <hidayath@linux.ibm.com>
    Link: https://patch.msgid.link/20260730124227.167829-1-hidayath@linux.ibm.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    (cherry picked from commit fe79571f40434b257d68cbfb7b3ae93a794d8a11)

--- drivers/dibs/dibs_main.c
+++ drivers/dibs/dibs_main.c
@@ -137,6 +137,7 @@ struct dibs_dev *dibs_dev_alloc(void)
 	dibs = kzalloc(sizeof(*dibs), GFP_KERNEL);
 	if (!dibs)
 		return dibs;
+	spin_lock_init(&dibs->lock);
 	dibs->dev.release = dibs_dev_release;
 	dibs->dev.class = dibs_class;
 	device_initialize(&dibs->dev);
@@ -185,7 +186,6 @@ int dibs_dev_add(struct dibs_dev *dibs)
 	int i, ret;
 
 	max_dmbs = dibs->ops->max_dmbs();
-	spin_lock_init(&dibs->lock);
 	dibs->dmb_clientid_arr = kzalloc(max_dmbs, GFP_KERNEL);
 	if (!dibs->dmb_clientid_arr)
 		return -ENOMEM;
